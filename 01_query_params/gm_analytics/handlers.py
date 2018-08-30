7b797b17d0f19d617e5dc5522273e31f4f06cbbd
lorenaP <alperez9221@gmail.com>
2018-08-29 19:08:27 -0500

    cambioCarpetas

--- 01_query_params/gm_analytics/handlers.py
+++
@@ -1,5 +0,0 @@
-def get_user_info(username=None):
-    return {"user_info": username}
-
-def get_commits_info(username=None, time_range=None):
-    return {"time_range": time_range, "commits_count": 100}
