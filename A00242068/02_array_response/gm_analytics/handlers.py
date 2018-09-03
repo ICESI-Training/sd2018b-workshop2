666fccf67341cc946ec27400a7b6cef2a53e02fb
AlorenaP <alperez9221@gmail.com>
2018-09-03 14:47:18 -0500

    Update handlers.py

--- A00242068/02_array_response/gm_analytics/handlers.py
+++ A00242068/02_array_response/gm_analytics/handlers.py
@@ -1,9 +1,15 @@
+import logging
 def get_user_info(username=None):
     return {"user_info": username}
 
 def get_commits_info(username=None, time_range=None):
     return [{'commits_count': 10, 'yyyymmdd_date': '20180101'},
             {'commits_count': 10, 'yyyymmdd_date': '20180102'}]
-def get_commits_info(org=none, repos=none, usr=none):
-    return [{'org_info':org, 'repos_info':repos, 'usr_info':usr},
-            {'org_info':org, 'repos_info':repos, 'usr_info':usr}]
+
+def get_commits_infoUser(organization=None, repository=None, usrName=None):
+    logging.info('executing get_commits_infoUser method')
+    logging.debug('organization=%s', organization)
+    logging.debug('repository=%s', repository)
+    logging.debug('usrName=%s', usrName)
+    return [{'org_info':organization, 'repos_info':repository, 'usr_info':usrName},
+            {'org_info':organization, 'repos_info':repository, 'usr_info':usrName}]
