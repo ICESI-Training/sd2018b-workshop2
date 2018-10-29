9783393281ec68b132f6d76efd85a8de176a1e04
daniel.barragan <daniel.barragan@correo.icesi.edu.co>
2018-10-29 18:04:10 -0500

    Add files

---
+++ scripts/post_install_py3.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+echo "export GM_ANALYTICS=/usr/local/lib/python3.6/site-packages/gm_analytics/swagger/indexer.yaml" >> ~/.bashrc
+# export PYTHONPATH=$PYTHONPATH:/usr/local/lib/python3.6/site-packages/
+# connexion run $GM_ANALYTICS --verbose -p 8088
