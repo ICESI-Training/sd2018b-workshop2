b6609f9a1f62abb44fbfe5fba069d007d3878a95
Daniel Barragán Calderón <d4n13lbc@users.noreply.github.com>
2018-10-29 19:08:26 -0500

    Update post_install_py3.sh

--- scripts/post_install_py3.sh
+++ scripts/post_install_py3.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-echo "export GM_ANALYTICS=/usr/local/lib/python3.6/site-packages/gm_analytics/swagger/indexer.yaml" >> ~/.bashrc
+echo "export GM_ANALYTICS=/usr/lib/python3.6/site-packages/gm_analytics/swagger/indexer.yaml" > ~/.bashrc
+echo "export LC_ALL=en_US.utf8" >> ~/.bashrc
+echo "export LANG=en_US.utf8" >> ~/.bashrc
+source ~/.bashrc
 pip3.6 install -r /usr/lib/python3.6/site-packages/gm_analytics-*-py3.6.egg-info/requires.txt
-# export PYTHONPATH=$PYTHONPATH:/usr/local/lib/python3.6/site-packages/
-# connexion run $GM_ANALYTICS --verbose -p 8088
